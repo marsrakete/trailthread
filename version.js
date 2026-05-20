af792df1ef67cb3517c127cd4b46170089dc8511
millux <markus@marsrakete.de>
2026-05-20 21:21:45 +0200

    Add files via upload

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 globalThis.APP_VERSION_INFO = Object.freeze({
-  appVersion: "0.5.66",
-  cacheVersion: "v70",
+  appVersion: "0.5.67",
+  cacheVersion: "v71",
   label: "Offline track library",
 });
